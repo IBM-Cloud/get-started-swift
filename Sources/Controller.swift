1f89eaf6ef1ee7b6b651225708ccef0f077cf05a
Ricardo Olivieri <rolivieri@users.noreply.github.com>
2017-02-24 18:23:12 -0600

    refactoring (#3)
    * refactoring
    
    * wrong json file
    
    * Refactoring code
    
    * API should handle no db
    
    * create new db
    
    * Look up cloudant service by label, not name

---
+++ Sources/Controller.swift
@@ -0,0 +1,178 @@
+/*
+* Copyright IBM Corporation 2017
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import Foundation
+import Kitura
+import SwiftyJSON
+import LoggerAPI
+import CloudFoundryEnv
+import CouchDB
+
+enum ServerError : Error {
+    case RuntimeError
+    // other types of errors...
+}
+
+public class Controller {
+
+  let router: Router
+  let appEnv: AppEnv
+  let dbName = "mydb"
+  var database: Database?
+
+  var port: Int {
+    get { return appEnv.port }
+  }
+
+  init() throws {
+    // Get environment variables from config.json or VCAP_SERVICES
+    let configFile = URL(fileURLWithPath: #file).appendingPathComponent("../config.json").standardized
+    if let configData = try? Data(contentsOf: configFile), let configJson = try JSONSerialization.jsonObject(with: configData, options: []) as? [String : Any] {
+      Log.info("Configuration file found: \(configFile)")
+      appEnv = try CloudFoundryEnv.getAppEnv(options: configJson)
+    } else {
+      Log.info("No configuration file found... using environment variables.")
+      appEnv = try CloudFoundryEnv.getAppEnv()
+    }
+
+    // Get database connection details...
+    
+    let services = appEnv.getServices()
+    let servicePair = services.filter { element in element.value.label == "cloudantNoSQLDB" }.first
+
+    if let cloudantService = servicePair?.value {
+      guard let credentials = cloudantService.credentials,
+        let dbHost = credentials["host"] as? String,
+        let dbUsername = credentials["username"] as? String,
+        let dbPassword = credentials["password"] as? String,
+        let dbPort = credentials["port"] as? Int else {
+          Log.error("Could not get credentials for Cloudant service.")
+          throw ServerError.RuntimeError
+      }
+
+      // Set up Connection to database
+      let connectionProperties = ConnectionProperties(host: dbHost,
+        port: Int16(dbPort),
+        secured: true,
+        username: dbUsername,
+        password: dbPassword)
+
+      let couchDBClient = CouchDBClient(connectionProperties: connectionProperties)
+      database = couchDBClient.database(dbName)
+      couchDBClient.createDB(dbName, callback: {
+           db, error in
+           if (db != nil) {
+               print("Created db")
+           }
+       })
+
+    } else {
+      database = nil
+      Log.warning("Could not find Cloudant service.")
+    }
+
+   // All web apps need a Router instance to define routes
+   router = Router()
+
+   router.all("/api/visitors", middleware: BodyParser())
+
+ /**
+  * Creates a new Visitor.
+  *
+  * REST API example:
+  * <code>
+  * POST http://localhost:8080/api/visitors
+  * <code>
+  * POST Body:
+  * <code>
+  * {
+  *   "name":"Bob"
+  * }
+  * </code>
+  */
+   router.post("/api/visitors") { request, response, next in
+     guard let parsedBody = request.body else {
+       next()
+       return
+     }
+
+     switch(parsedBody) {
+       case .json(let jsonBody):
+         let name = jsonBody["name"].string ?? ""
+         let json: [String: Any] = [ "name": name ]
+
+         if(self.database == nil) {
+            Log.error(">> No database")
+            response.status(.OK).send("Hello \(name)!")
+            break;
+         }
+         self.database?.create(JSON(json), callback: { (id: String?, rev: String?, document: JSON?, error: NSError?) in
+           if let _ = error {
+             Log.error(">> Could not persist document to database.")
+             response.status(.OK).send("Hello \(name)!")
+           } else {
+             Log.info(">> Successfully created the following JSON document in CouchDB:\n\t\(document)")
+             response.status(.OK).send("Hello \(name)! I added you to the database")
+           }
+         })
+
+       default:
+         break
+     }
+     next()
+   }
+
+ /**
+  * Gets all Visitors.
+  * REST API example:
+  * <code>
+  * GET http://localhost:8080/api/visitors
+  * </code>
+  *
+  * Response:
+  * <code>
+  * [ "Bob", "Jane" ]
+  * </code>
+  * @return An array of all the Visitors
+  */
+   router.get("/api/visitors") { _, response, next in
+     //If no database, return empty array.
+     guard let _ = self.database else {
+        Log.error(">> No database")
+        response.status(.OK).send(json: JSON([]))
+        next()
+        return
+     }
+     self.database?.retrieveAll(includeDocuments: true) { docs, error in
+       guard let docs = docs else {
+         Log.error(">> Could not read from database or none exists.")
+         response.status(.badRequest).send("Error could not read from database or none exists")
+         return
+       }
+       Log.info(">> Successfully retrived all docs from Database")
+
+       let names = docs["rows"].map { _, row in
+         return row["doc"]["name"].string ?? ""
+       }
+       response.status(.OK).send(json: JSON(names))
+       next()
+     }
+   }
+
+   router.all("/", middleware: StaticFileServer())
+ }
+
+}
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,159 +1,30 @@
 /*
- * Copyright IBM Corporation 2017
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-import Kitura
-import Foundation
+* Copyright IBM Corporation 2017
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 import LoggerAPI
 import HeliumLogger
-import CloudFoundryEnv
-import SwiftyJSON
-import CouchDB
+import Kitura
 
+// Init logger
 HeliumLogger.use(LoggerMessageType.info)
 
-//
-//default values. The DB values can get overwritten from VCAP_SERVICES env var or Sources/config.json
-
-var appPort = 8090
-var dbHost = ""
-var dbPort = Int16(443)
-var dbUsername = ""
-var dbPassword = ""
-var dbName = "mydb"
-
 do {
-   //
-   // get the app environment variables from config.json or VCAP_SERVICES
-
-   let appEnv: AppEnv
-   let configFile = URL(fileURLWithPath: #file).appendingPathComponent("../config.json").standardized
-   if let configData = try? Data(contentsOf: configFile), let configJson = try JSONSerialization.jsonObject(with: configData, options: []) as? [String:Any] {
-      Log.info("Configuration file found: \(configFile)")
-      appEnv = try CloudFoundryEnv.getAppEnv(options: configJson)
-   }
-   else {
-      Log.info("No configuration file found.")
-      appEnv = try CloudFoundryEnv.getAppEnv()
-   }
-
-   // get the port from configuration file
-   appPort = appEnv.port
-
-  //
-  // get database connection details from cfenv. This will look for VCAP_SERVICES first, and then config.json
-
-  let services = appEnv.getServices()
-  let servicePair = services.filter { element in element.value.label == "cloudantNoSQLDB" }.first
-
-  if let database = servicePair?.value {
-     print("Found the database! \(database)")
-
-     if let credentials = database.credentials {
-          dbHost = credentials["host"] as! String
-          dbUsername = credentials["username"] as! String
-          dbPassword = credentials["password"] as! String
-    }
-  } else {
-     print("Could not find your database.")
-  }
-
-} catch let error {
-  Log.error(error.localizedDescription)
+  let controller = try Controller()
+  Kitura.addHTTPServer(onPort: controller.port, with: controller.router)
+  Kitura.run()
+} catch {
   Log.error("Oops... something went wrong. Server did not start!")
-  fatalError()
 }
-
-//
-//Set up Connection to database
-
-let connectionProperties = ConnectionProperties(host: dbHost,
-                                                port: Int16(dbPort),
-                                                secured: true,
-                                                username: dbUsername,
-                                                password: dbPassword)
-
-let couchDBClient = CouchDBClient(connectionProperties: connectionProperties)
-
-var database = couchDBClient.database(dbName)
-couchDBClient.createDB(dbName, callback: {
-            db, error in
-            if (db != nil) {
-                database = db!
-            }
-        })
-
-//
-//Set routes and define logic
-
-let router = Router()
-
-router.all("/api/visitors", middleware: BodyParser())
-
-router.post("/api/visitors") { request, response, next in
-    guard let parsedBody = request.body else {
-        next()
-        return
-    }
-
-    switch(parsedBody) {
-    case .json(let jsonBody):
-            let name = jsonBody["name"].string ?? ""
-            let json: [String: Any] = [
-                                        "name": name
-                                      ]
-
-            database.create(JSON(json), callback: {
-                    (id: String?, rev: String?, document: JSON?, error: NSError?) in
-
-                    if let error = error {
-                        Log.error(">> Could not persist document to database.")
-                        response.status(.OK).send("Hello \(name)!")
-                    } else {
-                        Log.info(">> Successfully created the following JSON document in CouchDB:\n\t\(document)")
-                        response.status(.OK).send("Hello \(name)! I added you to the database")
-                    }
-            })
-
-    default:
-        break
-    }
-    next()
-}
-
-router.get("/api/visitors") { _, response, next in
-
-    database.retrieveAll(includeDocuments: true) { docs, error in
-
-      guard let docs = docs else {
-          Log.error(">> Could not read from database or none exists.")
-          response.status(.badRequest).send("Error could not read from database or none exists")
-          return
-      }
-
-      let names = docs["rows"].map { _, row in
-          return row["doc"]["name"].string ?? ""
-      }
-
-      response.status(.OK).send(json: JSON(names))
-   }
-   next()
-}
-
-
-router.all("/", middleware: StaticFileServer())
-
-Kitura.addHTTPServer(onPort: appPort, with: router)
-
-Kitura.run()
