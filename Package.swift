ccfb5ccb552a5bae81dc4e0b87a4ad8af6901741
rolivieri <roliv@us.ibm.com>
2017-03-01 11:07:50 -0600

    Fixed URL reference in Package.swift.

--- Package.swift
+++ Package.swift
@@ -21,6 +21,6 @@ let package = Package(
     .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1),
     .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
     .Package(url: "https://github.com/IBM-Swift/CloudConfiguration.git", majorVersion: 1),
-    .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB", majorVersion: 1, minor: 6)
+    .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 1, minor: 6)
 	]
 )
