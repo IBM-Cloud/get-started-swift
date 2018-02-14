ae1d58032bc951ec3636f4304ebc4f32e8224e4d
Aaron Liberatore <aaron.liberatore@gmail.com>
2018-02-14 15:58:22 -0600

    test

--- Sources/Configuration/Configuration.swift
+++ Sources/Configuration/Configuration.swift
@@ -16,16 +16,29 @@ public class ConfigManager {
     }
 
     public func getCloudantCredentials() -> CloudantCredentials? {
-        if let cloudant = services["cloudantNoSqlDB"] {
-            print(cloudant)
-        }
-        print(services)
+        if let service = services["cloudantNoSQLDB"] as? [String: Any] {
+           if let credentials = service["credentials"] as? [String: String] {
+               guard let url = credentials["url"], let username = credentials["username"], let password = credentials["password"] else {
+                   print("Could not cast", credentials)
+                   return nil
+               }
+               print("Success")
+               print(url)
+               print(username)
+               print(password)
+               return CloudantCredentials(url: url, username: username, password: password)
+           } else {
+               print("no credentials", service)
+           }
+       } else {
+           print("no cloudantNoSQLDB", services)
+       }
         return nil
     }
 
     private func load() {
         for (path, value) in ProcessInfo.processInfo.environment {
-            if path == "VCAP_SERVICES", let services = convertToDictionary(value)] {
+            if path == "VCAP_SERVICES", let services = convertToDictionary(value) {
                 self.services = services
                 break
             }
